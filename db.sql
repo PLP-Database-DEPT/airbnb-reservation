f27efe00535c94b7cd4d667c766aa98c9663c798
Kim-dr <kagasikimberly@gmail.com>
2025-09-04 21:39:04 +0300

    Added place_type table to categorize property types

--- db.sql
+++ db.sql
@@ -2,3 +2,12 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+
+-- Table: place_type
+-- This table stores records for the type of place a property can be categorized under.
+
+CREATE TABLE place_type (
+    id INT PRIMARY KEY AUTO_INCREMENT,  -- Unique identifier for each place type
+    type_name VARCHAR(100)              -- The name/description of the place type
+);
