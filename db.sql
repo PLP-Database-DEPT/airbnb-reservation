3d79c5079d94aa73aa59c72b08bcc4d704c29f4c
JackyChris12 <biwottjackline72@gmail.com>
2025-09-04 18:05:04 +0300

    Add user_review table with foreign keys to property and users

--- db.sql
+++ db.sql
@@ -2,3 +2,23 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Table: user_review
+-- This table stores reviews left by users on properties.
+-- Includes property, user, rating, comment, and date of review.
+-- Assumes 'property' table and 'users' table already exist.
+
+CREATE TABLE user_review (
+    id INT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    overall_rating INT NOT NULL,
+    comment TEXT,
+    review_date DATE,
+
+    -- Foreign key to property table
+    FOREIGN KEY (property_id) REFERENCES property(id),
+
+    -- Foreign key to users table (assumed to exist)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
