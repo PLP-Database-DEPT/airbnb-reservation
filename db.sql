48da482e69826e8d2ba8c492064f949e623611d5
PharesMwangi <mwasmwas683@gmail.com>
2025-09-04 19:55:30 +0300

    Created a booking status table with a unique identifier and booking status name

--- db.sql
+++ db.sql
@@ -2,3 +2,9 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Booking status table
+CREATE TABLE booking_status(
+id INT PRIMARY KEY, -- unique identifier
+status_name VARCHAR(100) -- name of the booking status
+);
