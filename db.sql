a913c1d84bf3d9ef0b3355cc80a4f4029d7dfa2d
Maliph-Guye <onditotonnis@gmail.com>
2025-09-04 23:08:24 +0300

    feat: create the property_type table with id and type_name columns

--- db.sql
+++ db.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Creating a property_type table to store types of properties
+CREATE TABLE property_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type_name VARCHAR(255)
+);
+
+-- Any other existing code...
