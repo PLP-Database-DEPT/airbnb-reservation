34f4bee26777dfa19a546fd97a71f84deba66ba4
Dee-GeoDev22 <moyadamacline@gmail.com>
2025-09-04 21:34:59 +0300

    Tackled issue 22

--- db.sql
+++ db.sql
@@ -2,3 +2,22 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Table: attribute_category
+-- Description: Stores categories for attributes
+
+CREATE TABLE attribute_category (
+    id INT PRIMARY KEY, -- Unique identifier for each category
+    category_name VARCHAR(255) NOT NULL
+);
+-- Table: attribute
+-- Description: Stores a list of all the attributes 
+-- that can be applied to a property.
+ 
+CREATE TABLE attribute (
+    id INT PRIMARY KEY, -- Unique identifier for each attribute
+    category_id INT NOT NULL, -- Links to attribute_category table
+    attribute_name VARCHAR(255) NOT NULL, -- Name of the attribute
+    description TEXT, -- Optional description of the attribute
+    FOREIGN KEY (category_id) REFERENCES attribute_category(id)
+);
