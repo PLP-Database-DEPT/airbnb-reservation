8ac3addbf5bf63b138c112d3e677d59c6f878e2c
StevenOyar <stvnoyaro@gmail.com>
2025-09-04 18:54:53 +0300

    Add region table with id and region_name columns

--- db.sql
+++ db.sql
@@ -2,3 +2,10 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+ -- Create region table to store world regions 
+CREATE TABLE region(
+id INT PRIMARY KEY AUTO_INCREMENT,  -- Unique identifier for each region
+region_name  VARCHAR(20)         -- Name of the region
+);
+
