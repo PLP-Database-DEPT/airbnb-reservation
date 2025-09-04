cbf25aa4313875e15a0d0004e2301370e4844ba8
Ajoke752 <ajokeabdulrasaq2@gmail.com>
2025-09-04 06:48:02 -0700

    Fix issue #18

--- db.sql
+++ db.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Create booking_guests table
+CREATE TABLE booking_guests (
+    booking_id INT,
+    guest_type_id INT,
+    num_guests INT,
+    FOREIGN KEY (booking_id) REFERENCES booking(id),
+    FOREIGN KEY (guest_type_id) REFERENCES guest_type(id)
