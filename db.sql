66e8f803751dbdff815e2a304cf1c9976be0de26
Your Name~ <you@example.com>
2025-09-04 21:49:22 +0300

    created location table with id, location name and country_id

--- db.sql
+++ db.sql
@@ -2,3 +2,10 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Creating the location table
+CREATE TABLE location(
+id INT PRIMARY KEY UNIQUE,
+country_id INT REFERENCES country(country_id) ON DELETE CASCADE,
+location_name VARCHAR(255) NOT NULL
+);
