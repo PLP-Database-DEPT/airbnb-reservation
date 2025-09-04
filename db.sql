991dd9813deaf58f80252ee15365f0bff3a41d38
maryashee <shikomaryk@gmail.com>
2025-09-04 22:49:09 +0300

    review_component

--- db.sql
+++ db.sql
@@ -2,3 +2,7 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+CREATE TABLE review_component (
+id INT PRIMARY KEY AUTO_INCREMENT,-- primary key is the unique for each row 
+component_name VARCHAR(100));
+SELECT * FROM review_component;
