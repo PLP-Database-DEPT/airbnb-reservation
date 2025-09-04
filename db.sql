10e45d962b8990a7fcef790f45ae795efeef6729
Noah Ewalan <106330276+ramnoa@users.noreply.github.com>
2025-09-04 21:52:18 +0300

    added customer, booking, bnbs , payment tables

--- db.sql
+++ db.sql
@@ -2,3 +2,30 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+-- customer table
+CREATE TABLE customer(
+  customerId INT PRIMARY KEY,
+  FirstName VARCHAR(200),
+  SurName  VARCHAR(200),
+  PhoneNo INT
+); 
+-- bnbs table
+CREATE TABLE bnbs(
+  bnbId INT PRIMARY KEY,
+  location VARCHAR(1000),
+  type VARCHAR(1000)
+);
+-- booking table
+CREATE TABLE booking(
+  bookingId INT AUTO_INCREMENT PRIMARY KEY,
+  customerId INT,
+  bnbId  INT ,
+  FOREIGN KEY (customerId) REFERENCE customer(customerId),
+  FOREIGN KEY (bnbId) REFERENCE bnbs(bnbId)
+); 
+-- payment table
+ CREATE TABLE payment(
+  paymentId INT PRIMARY KEY,
+  bookingId INT,
+  FOREIGN KEY (bookingId) REFERENCE booking(bookingId)
+ );
