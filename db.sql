1444ee98cbe384904bed8d25ca65ca8a326708ea
GeoffOminde <geoffominde8@gmail.com>
2025-09-04 20:08:44 +0300

    Add category table to db.sql with id and category_name

--- db.sql
+++ db.sql
@@ -1,4 +1,11 @@
--- create airbnb database
-CREATE DATABASE airbnb;
--- work with airbnb database
-USE airbnb;
+
+-- Table: category
+-- Description:
+-- This table stores different categories that can be applied to a property.
+-- Each category has a unique identifier (auto-incremented) and a category name.
+-- -----------------------------------
+
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for each category (auto-generated)
+    category_name VARCHAR(255) NOT NULL -- Name of the category
+);
