59d419178c54ee74b3d3c8ae295bd54e1f00a9aa
edmondweb <edmondbonventure@gmail.com>
2025-09-05 11:12:11 +0300

    Created country table with foreign key reference to region.

--- db.sql
+++ db.sql
@@ -1,4 +1,15 @@
+-- Active: 1753875628157@@127.0.0.1@3306@airbnb
 -- create airbnb database
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- Create country table
+
+CREATE TABLE country (
+    id INT PRIMARY KEY,      -- Unique identifier for each country
+    region_id INT,            -- Foreign Key linking to the 'region' table
+    country_name VARCHAR(255),      -- Name of the country
+    CONSTRAINT fk_region FOREIGN KEY (region_id)     -- Foreign Key constraint to ensure that the region_id refers to a valid region in the 'region' table
+    REFERENCES region(id)
+);
