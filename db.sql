a40d6bff61046dfd70fa38eef67c0d3bfd5652d5
hismyhill <owolabismaeel@gmail.com>
2025-09-05 11:50:27 +0100

    create property_attribute table

--- db.sql
+++ db.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- creat property_attribute table
+CREATE TABLE property_attribute(
+    property_id INT,
+    attribute_id INT,
+    FOREIGN KEY (property_id) REFERENCES property(property_id)
+    FOREIGN KEY (attribute_id) REFERENCES attribute(attribute_id)
+)
