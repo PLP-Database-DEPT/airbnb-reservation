8824590f3c078beeb88acd654c9db99a9ef4be67
Henryoile <henryoile627@gmail.com>
2025-09-05 13:48:04 +0300

    Add property_category table linking property and category tables

--- db.sql
+++ db.sql
@@ -2,3 +2,11 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+-- create property_category table to link properties and categories
+CREATE TABLE property_category (
+    property_id INT,
+    category_id INT,
+    FOREIGN KEY (property_id) REFERENCES property(property_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
