70fa8f8e62ada9cfd8179fe3799978769db686e7
VictorOduorke <victoroduorxh2@gmail.com>
2025-09-06 21:25:52 +0300

    created the component_rating table with the following attributes component_id, review_id,rating

--- db.sql
+++ db.sql
@@ -2,3 +2,18 @@
 CREATE DATABASE airbnb;
 -- work with airbnb database
 USE airbnb;
+
+---create table component_rating
+--This component table acts as a joining table between the review_component and user_review table
+
+CREATE TABLE component_rating(
+    component_id INT,
+    review_id INT,
+    rating INT,
+
+    ---This foreign key connects to the review_component table
+    FOREIGN KEY (component_id) REFERENCES review_component(id),
+
+    ---This foreign key connects to the user_review table
+    FOREIGN KEY (review_id) REFERENCES user_review(id)
+)
