0b942cf540e5b3548e5f0e1fa66fba0d2b096953
mokwathedeveloper <moffatmokwa12@gmail.com>
2025-09-08 13:22:44 +0300

    feat: Add foreign key constraints to property_category table

--- db.sql
+++ db.sql
@@ -17,3 +17,10 @@ CREATE TABLE `property_category` (
   `property_id` INT NOT NULL,
   `category_id` INT NOT NULL
 );
+
+--
+-- Constraints for table `property_category`
+--
+ALTER TABLE `property_category`
+  ADD CONSTRAINT `fk_property_category_property` FOREIGN KEY (`property_id`) REFERENCES `property` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_property_category_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE;
