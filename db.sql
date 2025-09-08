856ab3ca949f322ca493591a3d45d1ffbda580c5
Jeanne Wanjiru <71101608+Jeanne21@users.noreply.github.com>
2025-09-08 18:40:28 +0300

    Add booking table with foreign keys (#26)
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- db.sql
+++ db.sql
@@ -120,3 +120,19 @@ CREATE TABLE booking_status(
 id INT PRIMARY KEY,
 status_name VARCHAR(100) 
 );
+
+CREATE TABLE booking (
+id INT PRIMARY KEY,
+property_id INT NOT NULL,
+user_id INT NOT NULL,
+booking_status_id INT NOT NULL,
+checkin_date DATE NOT NULL,
+checkout_date DATE NOT NULL,
+nightly_price DECIMAL(10,2) NOT NULL,
+service_fee DECIMAL(10,2) NOT NULL,
+cleaning_fee DECIMAL(10,2) NOT NULL,
+total_price DECIMAL(10,2) NOT NULL,
+FOREIGN KEY (property_id) REFERENCES property(id),
+FOREIGN KEY (user_id) REFERENCES user_account(id),
+FOREIGN KEY (booking_status_id) REFERENCES booking_status(id)
+);
